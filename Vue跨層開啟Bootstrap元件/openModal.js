f5a7a8e0974cacb1d9f2f6216c7dff374404d294
Danny-1211 <61547736+Danny-1211@users.noreply.github.com>
2022-04-16 18:33:33 +0800

    4/16 update

--- Vue跨層開啟Bootstrap元件/openModal.js
+++ Vue跨層開啟Bootstrap元件/openModal.js
@@ -1,14 +1,24 @@
 const app = Vue.createApp({
+  methods: {
+    openModal () {
+      this.$refs.modal.openModal();
+    }
+  }
+});
+app.component('modal' , {
   data () {
     return {
-
-    }
+      MyModal: '',
+    };
   },
   methods: {
-      
+    openModal () {
+      this.MyModal.show();
+    }
   },
-  mounted() {
-      
+  mounted () {
+    this.MyModal = new bootstrap.Modal(this.$refs.textModal);
   },
+  template: '#modal'
 });
 app.mount('#app');
